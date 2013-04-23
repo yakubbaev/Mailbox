4f6de9d8e4bb600f06b41f87417dbd553b4d6532
Muhammad Yakubbaev <muhammad@Muhammads-MacBook-Pro.local>
2013-04-24 01:51:11 +0500

    Initial Commit

---
+++ Mailbox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mailbox
+//
+//  Created by Muhammad Yakubbaev on 4/24/13.
+//  Copyright (c) 2013 Muhammad Yakubbaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
